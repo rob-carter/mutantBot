761e67546fd24ac51b17c59dd2027c3b973723f7
Rob Carter <woweezowee@Red-Gazelle.local>
2022-06-09 04:21:02 -0600

    added events to start bot and parse commands

---
+++ events/interactionCreate.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'interactionCreate',
+	execute(interaction) {
+		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+        if (!interaction.isCommand()) return;
+        const command = interaction.client.commands.get(interaction.commandName);
+        if (!command) return;
+        try { 
+            command.execute(interaction); 
+        } catch {
+            console.error(error);
+            interaction.reply({content: 'There was an error while executingthis command!', ephemeral: true})
+        }
+	},
+};
