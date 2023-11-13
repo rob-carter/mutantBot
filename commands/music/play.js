095097e503ba3e7c6e0f851524e1a0ece6169407
Rob <robear95@icloud.com>
2023-11-13 16:27:44 -0700

    removed periods

--- commands/music/play.js
+++ commands/music/play.js
@@ -3,9 +3,9 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('play')
-        .setDescription('play a song from source.')
+        .setDescription('play a song from source')
         .addStringOption(option =>
-            option.setName('source').setDescription('the thing you want to play.').setRequired(true)
+            option.setName('source').setDescription('the thing you want to play').setRequired(true)
         ),
     async execute(interation) {
         const input = interation.options.getString('source');
--- commands/temp/server.js
+++ commands/temp/server.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
 		.setName('server')
-		.setDescription('provides information about the server.'),
+		.setDescription('provides information about the server'),
     async execute(interaction) {
-        await interaction.reply(`this server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+        await interaction.reply(`this server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members`);
         },
 };
