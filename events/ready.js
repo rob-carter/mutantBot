1c5905b364872cf70100738c7bafd913fcd3ac06
Rob <robear95@icloud.com>
2023-11-13 17:18:50 -0700

    testing distube events

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,13 @@ module.exports = {
     once: true,
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
+
+        client.distube.on('error', (channel, error) => {
+            console.error(error)
+        })
+        client.distube.on('PLAY_SONG', (channel, song) => {
+            console.log(channel)
+        })
+
         },
 };
