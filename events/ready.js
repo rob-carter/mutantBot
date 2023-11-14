45e78ba4e9397cc1511d4712edf15cdce6c77295
Rob <robear95@icloud.com>
2023-11-13 17:55:59 -0700

    added time to log for each song played

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
-const { Events } = require('discord.js');
+const { Events, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
 
 module.exports = {
     name: Events.ClientReady,
@@ -6,11 +7,19 @@ module.exports = {
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
 
-        client.distube.on('error', (channel, error) => {
-            console.error(error)
-        })
-        client.distube.on('PLAY_SONG', (channel, song) => {
-            console.log(channel)
+//        client.distube.on('error', (channel, error) => {
+//            console.error(error)
+//        })
+
+        client.distube.on('playSong', (queue, song) => {
+            var currentdate = new Date();
+            var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/"
+                + currentdate.getFullYear() + " @ "
+                + currentdate.getHours() + ":"
+                + currentdate.getMinutes()
+
+            console.log('* ', song.name, song.url, datetime)
         })
 
         },
