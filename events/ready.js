b41c55bd758fd42c1aa03b0a3851b8a2cf835545
Rob <robear95@icloud.com>
2023-11-15 16:39:53 -0700

    added event enitter for accurate reply in play command

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,7 @@
-const { Events, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { Events } = require('discord.js');
+const { EventEmitter } = require('events');
 
+const eventEmitter = new EventEmitter();
 
 module.exports = {
     name: Events.ClientReady,
@@ -7,20 +9,23 @@ module.exports = {
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
 
-//        client.distube.on('error', (channel, error) => {
-//            console.error(error)
-//        })
+        client.distube.on('error', (channel, error) => {
+            console.error(error)
+        })
 
         client.distube.on('playSong', (queue, song) => {
             var currentdate = new Date();
             var datetime = currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/"
+                + (currentdate.getMonth() + 1) + "/"
                 + currentdate.getFullYear() + " @ "
                 + currentdate.getHours() + ":"
                 + currentdate.getMinutes()
 
-            console.log('* ', song.name, song.url, datetime)
-        })
+            console.log('* ', song.name, song.url, datetime);
+            eventEmitter.emit('playSong', queue, song);
+        });
 
         },
 };
+
+module.exports.eventEmitter = eventEmitter;
