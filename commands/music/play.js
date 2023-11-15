f736ba7a7224ef41a0b146bb654d1348b6b49fce
Rob <robear95@icloud.com>
2023-11-15 16:44:26 -0700

    updated play to use event emit to accurately return the correct song

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,4 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { eventEmitter } = require('../../events/ready');
+require('dotenv').config();
+
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,10 +10,39 @@ module.exports = {
         .addStringOption(option =>
             option.setName('source').setDescription('the thing you want to play').setRequired(true)
         ),
-    async execute(interation) {
-        const input = interation.options.getString('source');
-        const voice = interation.guild.members.cache.get(interation.member.user.id).voice.channel;
+    async execute(interaction) {
+        await interaction.deferReply();
 
-        await interation.client.distube.play(voice, input, { skip: true });
+        const voice = interaction.guild.members.cache.get(interaction.member.user.id).voice.channel;
+
+        if (!voice) {
+            await interaction.reply({ content: 'you must be in a voice channel before playing a song', ephemeral: true });
+            return;
+        }
+
+        let input = interaction.options.getString('source');
+        let isTurkey = false;
+
+        if (input === 'turkey') {
+            input = process.env.TURKEY
+            isTurkey = true;
+        }
+
+        eventEmitter.once('playSong', (queue, song) => {
+            setTimeout(function() {}, 5000)
+            if (isTurkey) {
+                interaction.editReply(`now playing: [turkey neckin](<${process.env.TURKEY}>)`)
+            } else {
+                interaction.editReply(`now playing: [${song.name}](<${song.url}>)`);
+            }
+
+        });
+
+        await interaction.client.distube.play(voice, input, { skip: true });
+
+        if (isTurkey) {
+            await interaction.client.distube.shuffle(interaction.guild.id);
+            await interaction.client.distube.skip(interaction.guild.id);
+        }
     }
-}
+};
