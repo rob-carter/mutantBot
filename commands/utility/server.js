d4238be6017785210abc41bfec5fb187e9561fd2
Rob Carter <robear95@icloud.com>
2025-03-08 23:10:53 -0700

    deleted server command

--- commands/utility/server.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('server')
-        .setDescription('provides information about the server.'),
-    async execute(interaction) {
-        await interaction.reply(`server name: ${interaction.guild.name}\ntotal members: ${interaction.guild.memberCount}`);
-    },
-};
