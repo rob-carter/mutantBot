252c5a7f65554dc6da456f7b91b44611a7850f1c
Rob Carter <robear95@icloud.com>
2025-03-08 23:10:28 -0700

    basic playback working

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,9 +1,31 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { useMainPlayer } = require('discord-player');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('play')
-		.setDescription('play from a given source.'),
+		.setDescription('play from a given source.')
+		.addStringOption(option =>
+			option.setName('query')
+				.setDescription('the query to search for')
+				.setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply('todo');
-	},
+		const player = useMainPlayer();
+		const channel = interaction.member.voice.channel;
+		if (!channel)
+			return interaction.reply('you need to be in a voice channel!');
+		const query = interaction.options.getString('query', true);
+		await interaction.deferReply();
+		try {
+			const { track } = await player.play(channel, query, {
+				nodeOptions : {
+					metadata: interaction
+				}
+			});
+			console.log(track);
+			return interaction.followUp(`${track.title} has been added to the queue!`);
+		} catch (error) {
+			console.error(error);
+			return interaction.followUp('something went wrong');
+		}
+	}
 };
--- index.js
+++ index.js
@@ -1,43 +1,50 @@
 const fs = require('fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Player } = require('discord-player');
+const { DefaultExtractors } = require('@discord-player/extractor');
 const dotenv = require('dotenv');
 dotenv.config();
 const token = process.env.TOKEN;
-const client = new Client({ 
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.GuildVoiceStates,
-    ]
-});
-client.commands = new Collection();
-const folderPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(folderPath);
-for (const folder of commandFolders) {
-    const commandsPath = path.join(folderPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+async function main() {
+    const client = new Client({ 
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.GuildMembers,
+            GatewayIntentBits.GuildMessageReactions,
+            GatewayIntentBits.GuildVoiceStates,
+        ]
+    });
+    const player = new Player(client);
+    await player.extractors.loadMulti(DefaultExtractors);
+    client.commands = new Collection();
+    const folderPath = path.join(__dirname, 'commands');
+    const commandFolders = fs.readdirSync(folderPath);
+    for (const folder of commandFolders) {
+        const commandsPath = path.join(folderPath, folder);
+        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+        for (const file of commandFiles) {
+            const filePath = path.join(commandsPath, file);
+            const command = require(filePath);
+            if ('data' in command && 'execute' in command) {
+                client.commands.set(command.data.name, command);
+            } else {
+                console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            }
         }
     }
-}
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args, client));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args, client));
+    const eventsPath = path.join(__dirname, 'events');
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+    for (const file of eventFiles) {
+        const filePath = path.join(eventsPath, file);
+        const event = require(filePath);
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args, client));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args, client));
+        }
     }
+    client.login(token); 
 }
-client.login(token);
+main().catch(console.error);
