f23f899c936e0db5d943ddf6967b5b4249fdcca5
Rob Carter <robear95@icloud.com>
2025-03-25 17:03:06 -0600

    fix: streamline play command to handle track playback and queue insertion

--- commands/music/play.js
+++ commands/music/play.js
@@ -15,45 +15,16 @@ module.exports = {
 		let queue = useQueue(interaction.guild.id);
 		const channel = interaction.member.voice.channel;
 		if (!channel)
-			return interaction.reply('you need to be in a voice channel!');
-        if (!queue) {
-            queue = player.nodes.create(interaction.guild, {
-                metadata: interaction
-            });
-        }
+			return interaction.reply('you need to be in a voice channel!'); 
 		const query = interaction.options.getString('query', true);
 		await interaction.deferReply();
-		if (query === 'auto') {
-			try {
-				await player.play(channel, process.env.AUTO, {
-					nodeOptions : {
-						metadata: interaction
-					}
-				});
-				queue.tracks.shuffle();
-				return interaction.followUp('playing autoplaylist!');
-			} catch (error) {
-				console.error(error);
-				return interaction.followUp('something went wrong with autoplaylist.');
-			}
-		}
 		try {
-			// const { track } = await player.play(channel, query, {
-			// 	nodeOptions : {
-			// 		metadata: interaction
-			// 	}
-			// });
-			// return interaction.followUp(`${track.title} has been added to the queue!`);
-			track = await player.search(query, { requestedBy: interaction.user });
-			queue.insertTrack(track.tracks[0]);
-			if (!queue.isPlaying()) {
-				await player.play(channel, "https://www.youtube.com/watch?v=1VXMLuZkq1g", { //TODO: remove this hack
-					nodeOptions : {
-						metadata: interaction
-					}
-				});
-			}
-			return interaction.followUp(`${track.tracks[0].title} has been added to the queue!`);
+			const { track } = await player.play(channel, query, {
+				nodeOptions : {
+					metadata: interaction
+				}
+			});
+			return interaction.followUp(`${track.title} has been added to the queue!`);
 		} catch (error) {
 			console.error(error);
 			return interaction.followUp('something went wrong with playing that track.');
